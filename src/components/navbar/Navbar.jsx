23dc2abe263dcf78a38b9206a39c6da1375c1614
Vlad Culda <culdavlad93@gmail.com>
2022-09-16 11:12:44 +0300

    added sidebar and navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './navbar.scss';
+import SearchIcon from '@mui/icons-material/Search';
+import LanguageIcon from '@mui/icons-material/Language';
+import DarkModeOutlineIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitIcon from '@mui/icons-material/FullscreenExit';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import ListOutlineIcon from '@mui/icons-material/ListOutlined';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='wrapper'>
+        <div className='search'>
+          <input type='text' placeholder='Search...' />
+          <SearchIcon />
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <LanguageIcon className='icon' />
+            English
+          </div>
+          <div className='item'>
+            <DarkModeOutlineIcon className='icon' />
+          </div>
+          <div className='item'>
+            <FullscreenExitIcon className='icon' />
+          </div>
+          <div className='item'>
+            <NotificationsNoneIcon className='icon' />
+            <div className='counter'>1</div>
+          </div>
+          <div className='item'>
+            <ChatBubbleOutlineIcon className='icon' />
+          </div>
+          <div className='item'>
+            <ListOutlineIcon className='icon' />
+          </div>
+          <div className='item'>
+            <img
+              src='https://images.pexels.com/photos/11293719/pexels-photo-11293719.jpeg?auto=compress&cs=tinysrgb&w=1600'
+              alt=''
+              className='avatar'
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
